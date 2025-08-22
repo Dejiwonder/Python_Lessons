f0ed02ed8aba41eb95799dc458b96efc914ba225
Dejiwonder <princeadedejiadeyemi@gmail.com>
2025-08-22 20:25:17 +0200

    New Knowldege

---
+++ package/maths.py
@@ -0,0 +1,4 @@
+def addition(a, b):
+    """Returns the sum of a and b."""
+    return a + b
+# This is our module for basic math operations.
---
+++ test.py
@@ -0,0 +1,4 @@
+from package.maths import * # we are importing from the new module we created inn our package
+print( addition(25, 25))  # Use the addition function from the package
+
+
